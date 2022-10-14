88a1c458e551f2f08901b46b121899c35c134239
Theresa Denz <tdenz@gmx.at>
2022-10-14 09:30:35 +0200

    finished assignment 01

---
+++ JS_Basics/01 JS Variablen_Operationen/script.js
@@ -0,0 +1,28 @@
+// Variablen
+let a = 7;
+let b = 8;
+let c = 10;
+// Berechne folgendes und gib das Eregebnis über console.log aus
+let result;
+result = a + b + c;
+console.log(result);
+
+result = (a + b) / c;
+console.log(result);
+
+result = c - a + b;
+console.log(result);
+
+result = a + b + c * 3;
+console.log(result);
+
+result = (a + b + c)/4;
+console.log(result);
+
+// Ändere die Variablen auf folgende Werte
+a = 3
+b = a + 3
+c = c -3
+
+console.log("Die neuen Werte sind a=" + a + " b=" + b + " c=" + c);
+// gib die neuen Werte der Variablen aus
