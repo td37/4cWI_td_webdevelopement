fc1e975a4f2a39282769bffd9b60a25ac800fea4
Theresa Denz <tdenz@gmx.at>
2022-10-14 09:30:44 +0200

    finished assignment 02

---
+++ JS_Basics/02 JS Strings_Zahlen/script.js
@@ -0,0 +1,10 @@
+//Berechne das Ergebnis von 17.4 / 3.9. Gib folgenden Satz aus:
+//The result of 17.4 divided by 3.9 is 4.461538461538462
+
+let result = 17.4/3.9;
+console.log("The result of 17.4 divided by 3.9 is " + result);
+
+//Lege eine Variable firstname und eine Varialbe lastname an. Weise den Variablen Deinen Namen zu. Gib folgendes aus:
+let firstname = "Theresa";
+let lastname = "Denz";
+console.log("Ich heiße " + firstname + " " + lastname);
