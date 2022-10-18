8c19549176e0c0e90664324882f0837898c0cb6f
Theresa Denz <tdenz@gmx.at>
2022-10-18 12:12:03 +0200

    finished assignment 05

---
+++ JS_Basics/05 If-Condition/script.js
@@ -0,0 +1,31 @@
+// erstelle eine Variable number
+let number = 7;
+
+// Aufgabe 1
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini"
+if (number < 20) {
+    console.log("mini");
+}
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
+if (number >= 20 && number <= 50) {
+    console.log("medium");
+}
+// Wenn die Zahl größer als 50 ist gib aus "Large"
+if (number > 50) {
+    console.log("large");
+}
+
+
+// Aufgabe 2
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini"
+if (number<=7){
+    console.log("mini");
+}
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium"
+if (number>7&&number<90){
+    console.log("medium");
+}
+// Wenn die Zahl größer als 90 ist gib aus "Large"
+if (number>=90){
+    console.log("large");
+}
