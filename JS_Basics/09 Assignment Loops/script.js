f2fb63c9b262a85d5a5e1ac27d5255b73bf291b0
Theresa Denz <tdenz@gmx.at>
2022-11-08 11:43:02 +0100

    finished assignment 09

--- JS_Basics/09 Assignment Loops/script.js
+++ JS_Basics/09 Assignment Loops/script.js
@@ -2,25 +2,26 @@
 // X X X X 
 // O O O O 
 // X X X X 
-// O O O O 
+// O O O O
+
 for (let i = 0; i<4; i++){
     let output = "";
     for(let j = 0; j<4; j++){
         if(i%2!=0){
-            output  += "X";
+            output  += "O";
         }
         else {
-            output += "O"
+            output += "X"
         }
        
     }
     console.log(output);
 }
 // 2. Versuche in einer for - Schleife (1-100) alle geraden Zahlen zu addieren (zwei Lösungswege ⇒ Zähler dahingehend verändern das er nur gerade Zahlen zählt und mit Modulo arbeiten.) 
+let sum = 0;
+for (let i = 0; i <=100; i+=2) {
+    sum += i;   
+}
+console.log("Die Summe aller geraden Zahlen zwischen 1 und 100 ist " + sum)
 
-// 3. Versuche mit einer for/while Schleife den Anfangsbuchstaben deines Namens auszugeben, z.B.: 
-// XX X XX X 
-// X    X X   X 
-// X            X
-// X            X
 
