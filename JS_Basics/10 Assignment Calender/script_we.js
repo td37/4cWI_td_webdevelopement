77b6ee29ec77dd57ab5d5bdab8f4f814cd28f369
Theresa Denz <tdenz@gmx.at>
2022-11-18 08:22:32 +0100

    finished assignment 10

---
+++ JS_Basics/10 Assignment Calender/script_we.js
@@ -0,0 +1,85 @@
+let month = 2;
+let monthLength = 0;
+let startDayOfWeek = 3;
+let dayOfMonth = 1;
+let empty = "|    ";
+let daysInAWeek = 7;
+const daysOnAPage = 35;
+let weekend = 0;
+let header = "| MO | DI | MI | DO | FR | SA | SO |"
+
+switch (month) {
+    case 1:
+        monthLength = 31;
+        break;
+    case 2:
+        monthLength = 28;
+        break;
+    case 3:
+        monthLength = 31;
+        break;
+    case 4:
+        monthLength = 30;
+        break;
+    case 5:
+        monthLength = 31;
+        break;
+    case 6:
+        monthLength = 30;
+        break;
+    case 7:
+        monthLength = 31;
+        break;
+    case 8:
+        monthLength = 31;
+        break;
+    case 9:
+        monthLength = 30;
+        break;
+    case 10:
+        monthLength = 31;
+        break;
+    case 11:
+        monthLength = 30;
+        break;
+    case 12:
+        monthLength = 31;
+        break;
+}
+
+console.log(header);
+
+let outputPerWeek = "";
+
+
+for (let j = 1; j <= daysOnAPage; j++) {
+    weekend++;
+    if (j < startDayOfWeek || dayOfMonth > monthLength) {
+        outputPerWeek += empty;
+    }
+    else {
+        if (weekend == 6 || weekend == 7) {
+            outputPerWeek += "| WE ";
+            if (weekend == 7) {
+                weekend = 0;
+            }
+
+        }
+        else if (dayOfMonth < 10) {
+            outputPerWeek += "|  " + dayOfMonth + " ";
+            dayOfMonth++;
+        }
+        else {
+            outputPerWeek += "| " + dayOfMonth + " ";
+            dayOfMonth++;
+        }
+    }
+    if (j % 7 == 0) {
+        outputPerWeek += "|";
+        console.log(outputPerWeek);
+        outputPerWeek = "";
+    }
+
+}
+
+
